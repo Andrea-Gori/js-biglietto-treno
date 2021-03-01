4b826c6e17b003e177536479fc06a59e3e6927ce
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-03-01 16:06:32 +0100

    completo, inizio rifiniture con css

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+alert("Gentile cliente, ti invitiamo ad inserire i tuoi dai per poter calcolare il prezzo del tuo biglietto.");
+
+var distanza = parseFloat(prompt('Qui inserisci la distanza da percorrere in Km.'));
+
+var eta = parseInt(prompt('Qui inserisci la tua età.'));
+
+var costo = 0.21
+
+var biglietto = distanza * costo;
+
+if (eta < 18) {
+  alert('Il prezzo del suo biglietto è:' + (biglietto - (biglietto * 0.20)).toFixed(2) + ' €');
+  console.log('Il prezzo del suo biglietto è: ' + (biglietto - (biglietto * 0.20)).toFixed(2) + ' €');
+} else if (eta > 65) {
+  alert('Il prezzo del suo biglietto è:' + (biglietto - (biglietto * 0.40)).toFixed(2) + ' €')
+  console.log('Il prezzo del suo biglietto è: ' + (biglietto - (biglietto * 0.40)).toFixed(2) + ' €');
+} else {
+  alert('Il prezzo del suo biglietto è:' + biglietto.toFixed(2) + '€')
+  console.log('Il prezzo del suo biglietto è: ' + biglietto.toFixed(2) + '€');
+}
